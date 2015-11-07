9d06817cf18e8fcf6c678e8ebc0bf7eba941db56
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2015-11-07 14:15:34 +0100

    switch to birth and school bg added

--- main.lua
+++ main.lua
@@ -1,16 +1,21 @@
 background = nil
-clicked = true
+clicked = false
+stage = 0
 
 local function onItem(x, y)
     if y >= 400 and y < 500 then
         if x >= 100 and x < 200 then
             item = 1
+            clicked = true
         elseif x >= 300 and x < 400 then
             item = 2
+            clicked = true
         elseif x >= 500 and x < 600 then
             item = 3
+            clicked = true
         elseif x >= 700 and x < 800 then
             item = 4
+            clicked = true
         end
     end
     return item
@@ -18,11 +23,13 @@ end
 
 function love.load()
     love.window.setMode(900, 600)
-	bg1 = love.graphics.newImage("Background1.png")
-	bg2 = love.graphics.newImage("Background2.png")
-	smile = love.graphics.newImage("smile.png")
-	love.graphics.setColor(255,255,255,255)
-	background = bg1
+    bg1 = love.graphics.newImage("Background1.png")
+    bg2 = love.graphics.newImage("Background2.png")
+    bgBirth = love.graphics.newImage("BirthBackground.png")
+    bgSchool = love.graphics.newImage("SchoolBackground.png")
+    smile = love.graphics.newImage("smile.png")
+    love.graphics.setColor(255,255,255,255)
+    background = bg1
 end
 
 function love.update(dt)
@@ -31,17 +38,31 @@ function love.update(dt)
 end
 
 function love.mousepressed(x,y,button)
-    if button == 'l' and not clicked and onItem(x, y) ~= nil  then
-        background = bg2
-        imgx = x
-        imgy = y
-        clicked = true
-    elseif button == 'l' and clicked and onItem(x, y) ~= nil then
-        background = bg1
-        imgx = x
-        imgy = y
-        clicked = false
+    if button == 'l' then
+        -- satrt the game load the first background
+        if stage == 0 then
+            stage = 1
+            background = bgBirth
+        -- choose item for the first stage
+        elseif stage == 1 and onItem(x,y) ~= nil and clicked then
+            stage = 2
+            background = bgSchool
+        -- elseif stage == 2 then
+        --     background = bgSchool
+        --     stage = 3
+        end
     end
+    -- if  and not clicked and onItem(x, y) ~= nil  then
+    --     background = bg2
+    --     imgx = x
+    --     imgy = y
+    --     clicked = true
+    -- elseif button == 'l' and clicked and onItem(x, y) ~= nil then
+    --     background = bg1
+    --     imgx = x
+    --     imgy = y
+    --     clicked = false
+    -- end
 end
 
 function love.draw()
